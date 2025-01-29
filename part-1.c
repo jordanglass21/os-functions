9fb5d839a5c737fdae2e9ed51bb36bd808c82398
ubuntu <ubuntu@cs5600sp25-vm-jglass.khoury.northeastern.edu>
2025-01-29 17:25:38 +0000

    exit working?

--- part-1.c
+++ part-1.c
@@ -17,13 +17,37 @@ void exit(int err);
 /* Factor, factor! Don't put all your code in main()! 
 */
 
+// __NR_exit 60 (system call number)
+void exit(int err) {
+	// flush buffer data
+	
+	// close all open files
+
+	// remove temporary files
+
+	// return statusi
+	syscall(__NR_exit);
+
+	//if(err) {// exit failure
+	//	return 1;
+	//}
+	//return 0; // exit success
+}
+
 /* read one line from stdin (file descriptor 0) into a buffer: */
+// __NR_read 0
+int read(int fd, void *ptr, int len) {
+	return 1;
+}
 
 /* print a string to stdout (file descriptor 1) */
-
+// __NR_write 1 (system call number)
+int write(int fd, void *ptr, int len) {
+	return 1;
+}
 /* ---------- */
 
 void main(void)
 {
-	/* your code here */
+	exit(0);
 }
