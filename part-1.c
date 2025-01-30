5abdbf3b80335b46fe1ec91549302876376db7b6
lijich <ubuntu@cs5600sp25-vm-lijich.khoury.northeastern.edu>
2025-01-30 16:30:45 +0000

    looping and quit

--- part-1.c
+++ part-1.c
@@ -35,13 +35,45 @@ int write(int fd, void *ptr, int len) {
 	return syscall(__NR_write, fd, ptr, len);
 }
 
-int myPrint(void *buffer) {
-	int std_out = 1;
-	return write(std_out, buffer, bufSize);
+int myPrint(char *buffer, int bytesRead) {
+	int idx = 0;
+	char *s = buffer;
+	while(idx < bytesRead) {
+		write(1, s, 1);
+		s=s+1;
+		idx++;
+	}
+	buffer[idx++] = '\n';
+	buffer[idx] = '\0';
+	write(1, s, 2);
+	return idx;
 }
-int myScan(char *buffer) {
-	int std_in = 0;
-	return read(std_in, buffer, bufSize);
+int myScan(char *buffer, int bufferSize) {
+	char buf[2] = "> ";
+	write(1,buf,2);
+	int idx = 0;
+	char c = 's';
+	int byteRead = 0;
+	int qflag=0, uflag=0, iflag=0, tflag=0;
+
+	while(idx < bufferSize) {
+		byteRead = read(0, &c, 1);
+		
+		if(c == '\n') {
+			buffer[idx] = '\0';
+			break;
+		} else {
+			buffer[idx++] =  c;
+			if(c == 'q' && idx == 1){qflag = 1;}
+			if(c == 'u' && idx == 2){uflag = 1;}
+			if(c == 'i' && idx == 3){iflag = 1;}
+			if(c == 't' && idx == 4){tflag = 1;}
+		}
+	}
+	if(qflag && uflag && iflag && tflag){
+		exit(0);
+	}
+	return idx;
 }
 
 // helper functions
@@ -50,7 +82,8 @@ int myScan(char *buffer) {
 void main(void)
 {
 	char buf [bufSize];
-	myScan(buf);
-	myPrint(buf);
-	exit(0);
+	while(1) {
+		int bytesRead = myScan(buf, bufSize);
+		myPrint(buf, bytesRead);
+	}
 }
